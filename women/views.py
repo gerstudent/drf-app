5ae25a4a6d65a32040b6156abe0dd7a6431d1b27
gerstudent <gkolcherin@gmail.com>
2023-06-25 12:07:56 +0300

    Initial commit

---
+++ women/views.py
@@ -0,0 +1,48 @@
+from django.forms import model_to_dict
+from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Women
+from .serializers import WomenSerializer
+
+
+class WomenAPIView(APIView):
+    def get(self, request):
+        w = Women.objects.all()
+        return Response({'posts': WomenSerializer(w, many=True).data})
+
+    def post(self, request):
+        # валидация данных
+        serializer = WomenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        serializer.save()
+        return Response({'post': serializer.data})
+
+    def put(self, request, *args, **kwargs):
+        pk = kwargs.get("pk", None)
+        if not pk:
+            return Response({"error": "Method PUT not allowed"})
+        try:
+            instance = Women.objects.get(pk=pk)
+        except:
+            return Response({"error": "Object does not exists"})
+
+        serializer = WomenSerializer(data=request.data, instance=instance)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()  # вызовет метод update из serializers.py
+        return Response({"post": serializer.data})
+
+    def delete(self, request, *args, **kwargs):
+        pk = kwargs.get("pk", None)
+        if not pk:
+            return Response({"error": "Method DELETE not allowed"})
+
+        try:
+            obj = Women.objects.get(pk=pk)
+            obj.delete()
+        except:
+            return Response({"error": "Object does not exist"})
+        
+        return Response({"post": "delete post " + str(pk)})
